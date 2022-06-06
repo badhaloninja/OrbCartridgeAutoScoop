d163e9b98399c19aa862f733a80724234d0be013
badhaloninja <badhaloninja@badhalo.ninja>
2022-06-05 19:12:30 -0500

    Fixed issue of material conversion not working when "Pickup materials when created" is enabled

--- MaterialTipAutoScoop/MaterialTipAutoScoop.cs
+++ MaterialTipAutoScoop/MaterialTipAutoScoop.cs
@@ -11,7 +11,7 @@ namespace MaterialTipAutoScoop
     {
         public override string Name => "MaterialTipAutoScoop";
         public override string Author => "badhaloninja";
-        public override string Version => "1.2.0";
+        public override string Version => "1.2.1";
         public override string Link => "https://github.com/badhaloninja/MaterialTipAutoScoop";
 
         private static ModConfiguration config;
@@ -53,10 +53,10 @@ namespace MaterialTipAutoScoop
         [HarmonyPatch(typeof(MaterialTip), "GenerateMenuItems")]
         class MenuItems
         {
-            public static void Postfix(MaterialTip __instance, CommonTool tool, ContextMenu menu)
+            public static void Postfix(CommonTool tool, ContextMenu menu)
             {
                 Uri icon = new Uri("neosdb:///5f2cc0be2830560a76b6e2873e8e1dd86e4036a476656e693c90da19b4cf36cf.png");
-                ContextMenuItem contextMenuItem = menu.AddItem("What", icon, default(color));
+                ContextMenuItem contextMenuItem = menu.AddItem("What", icon, color.White);
 
                 var field = contextMenuItem.Slot.AttachComponent<ValueField<int>>();
                 field.Value.Value = config.GetValue(scoopMode);
@@ -105,8 +105,7 @@ namespace MaterialTipAutoScoop
 
                 if (instance.UsesLaser)
                 {
-                    bool hitSomething;
-                    mat = RaycastMaterial(instance, out hitSomething);
+                    mat = RaycastMaterial(instance, out bool hitSomething);
 
                     if (mat == null && !hitSomething)
                     {
@@ -126,34 +125,46 @@ namespace MaterialTipAutoScoop
         }
 
         // Scoop on created
-        //[HarmonyPatch(typeof(DevCreateNewForm), "SpawnMaterial")]
+        //[HarmonyPatch(typeof(DevCreateNewForm), "SpawnMaterial")] // Patched manually in OnEngineInit because of static constructor
         class DevCreateNewForm_SpawnMaterial_Patch
         {
             public static void Postfix(Slot slot, Type materialType)
             {
                 if (!config.GetValue(scoopOnCreated)) return;
-                // Get matarial tip from user if it exists and prioritize the primary hand
-                var commonTools = slot.LocalUserRoot.GetRegisteredComponents<CommonTool>(c => 
-                     c.ActiveToolTip is MaterialTip);
 
-                commonTools.Sort((a, b) => // Sort common tool by user primary hand 
-                    a.Side.Value == a.InputInterface.PrimaryHand ? -1 : 1);
 
-                var commonTool = commonTools.GetFirst();
-                if (commonTool == null) return; // Skip if no common tool with material tip found
+                /* Delay to allow for OnCreated actions to run
+                 * Since SpawnMaterial is a static method, I can't check if OnCreated is assigned to the CreateNewForm calling this method
+                 * So I have to delay every time
+                 * 
+                 * OnCreated actions like Convert to Material goes by the material in the tool tip when they run
+                 * and since this replaces the material in the tool tip *before* the OnCreated action runs it just copies the newly created material to it's self
+                 */
+                slot.RunInUpdates(0, () => // 0 is enough to run after OnCreated
+                {
+                    // Get matarial tip from user if it exists and prioritize the primary hand
+                    var commonTools = slot.LocalUserRoot.GetRegisteredComponents<CommonTool>(c =>
+                         c.ActiveToolTip is MaterialTip);
 
-                MaterialTip materialTip = commonTool.ActiveToolTip as MaterialTip;
-                materialTip.OrbSlot.DestroyChildren(filter: (orb) =>
-                {  // Destroy if setting == 0 |OR| if setting == 1 &AND& does not have material comp
+                    commonTools.Sort((a, b) => // Sort common tool by user primary hand 
+                        a.Side.Value == a.InputInterface.PrimaryHand ? -1 : 1);
+
+                    var commonTool = commonTools.GetFirst();
+                    if (commonTool == null) return; // Skip if no common tool with material tip found
+
+                    MaterialTip materialTip = commonTool.ActiveToolTip as MaterialTip;
+                    materialTip.OrbSlot.DestroyChildren(filter: (orb) =>
+                    {  // Destroy if setting == 0 |OR| if setting == 1 &AND& does not have material comp
                         int scoopSetting = config.GetValue(scoopMode);
-                    if (scoopSetting != 1) return scoopSetting == 0;
-                    return null == orb.GetComponent<IAssetProvider<Material>>();
+                        if (scoopSetting != 1) return scoopSetting == 0;
+                        return null == orb.GetComponent<IAssetProvider<Material>>();
+                    });
+
+                    materialTip.OrbSlot.ReparentChildren(slot.LocalUserSpace);
+
+                    slot.SetParent(materialTip.OrbSlot);
+                    slot.SetIdentityTransform();
                 });
-                
-                materialTip.OrbSlot.ReparentChildren(slot.LocalUserSpace);
-                
-                slot.SetParent(materialTip.OrbSlot);
-                slot.SetIdentityTransform();
             }
         }
     }
